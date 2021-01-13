e4af47bc07c66448fc87053f84c010a0a083f384
Răzvan Flavius Panda <razvan.panda@gmail.com>
2021-01-13 22:05:28 +0200

    refresh after other users submit

--- userscript.user.js
+++ userscript.user.js
@@ -7,56 +7,67 @@
 // @author      -
 // @description 3/8/2020, 8:42:28 PM
 // @require https://cdnjs.cloudflare.com/ajax/libs/ramda/0.27.1/ramda.min.js
-// @require https://raw.githubusercontent.com/lodash/lodash/4.17.15-npm/core.js
+// @require https://raw.githubusercontent.com/lodash/lodash/4.17.15-npm/lodash.js
 // ==/UserScript==
+// TODO
+// * easy way to view code side-by-side
 (new MutationObserver(check)).observe(document, { childList: true, subtree: true });
 function check(_changes, observer) {
     if (document.querySelector('.player-report')) {
         observer.disconnect();
-        var reports_1 = [];
-        $('.player-report').each(function (_, obj) {
-            reports_1.push({
-                rank: parseInt($(obj).find('.clash-rank').text()),
-                score: parseInt($(obj).find('div.info-clash.score > div > div.info-content-container > div.info-value > span').text()),
-                nickname: $(obj).find('.nickname').text(),
-                language: $(obj).find('div.info-clash.language > div > div.info-content-container > div.info-value > span').text()
+        var previousFinishedCount = 0;
+        setInterval(update, 3000);
+        function update() {
+            var reports = [];
+            $('.player-report').each(function (_, obj) {
+                reports.push({
+                    rank: parseInt($(obj).find('.clash-rank').text()),
+                    score: parseInt($(obj).find('div.info-clash.score > div > div.info-content-container > div.info-value > span').text()),
+                    nickname: $(obj).find('.nickname').text(),
+                    language: $(obj).find('div.info-clash.language > div > div.info-content-container > div.info-value > span').text()
+                });
             });
-        });
-        var isShortestMode = $('div.clash-info-container > div > div.info-clash.criterion > div > div.info-content-container > div.info-label > span').first().text() === 'CHARACTERS';
-        if (isShortestMode) {
-            var reportsByLanguage = R.groupBy(function (report) { return report.language; })(reports_1);
-            R.forEachObjIndexed(function (reports, _language) {
-                R.addIndex(R.forEach)(function (report, idx) { return report.fairRank = idx + 1; }, reports);
-            }, reportsByLanguage);
-            var fairReports_1 = _.sortBy(_.flatten(_.map(reportsByLanguage, function (reports, _) { return reports; })), function (report) { return report.rank; });
-            var worstRank_1 = _.max(_.map(fairReports_1, function (report) { return report.fairRank; }));
-            _.forEach(fairReports_1, function (report) {
-                if (report.score === 0) {
-                    report.fairRank = worstRank_1 + 1;
-                }
-            });
-            $('.clash-rank').each(function (index, obj) {
-                var fairReport = fairReports_1[index];
-                $(obj).text(fairReport.fairRank);
-                if (fairReport.score > 0) {
-                    var bgColor;
-                    switch (fairReport.fairRank) {
-                        case 1:
-                            bgColor = 'mediumseagreen';
-                            break;
-                        case 2:
-                            bgColor = 'yellow';
-                            break;
-                        default: bgColor = 'orange';
+            var finishedCount = _.countBy(reports, function (report) { return !isNaN(report.rank); }).true;
+            if (previousFinishedCount === finishedCount)
+                return;
+            previousFinishedCount = finishedCount;
+            var isShortestMode = $('div.clash-info-container > div > div.info-clash.criterion > div > div.info-content-container > div.info-label > span').first().text() === 'CHARACTERS';
+            if (isShortestMode) {
+                var reportsByLanguage = R.groupBy(function (report) { return report.language; })(reports);
+                R.forEachObjIndexed(function (reports, _language) {
+                    R.addIndex(R.forEach)(function (report, idx) { return report.fairRank = idx + 1; }, reports);
+                }, reportsByLanguage);
+                var fairReports_1 = _.sortBy(_.flatten(_.map(reportsByLanguage, function (reports, _) { return reports; })), function (report) { return report.rank; });
+                var worstRank_1 = _.max(_.map(fairReports_1, function (report) { return report.fairRank; }));
+                _.forEach(fairReports_1, function (report) {
+                    if (report.score === 0) {
+                        report.fairRank = worstRank_1 + 1;
                     }
-                }
-                else {
-                    bgColor = 'indianred';
-                }
-                $(obj)
-                    .parents("[ng-repeat='player in clashOfCodeService.currentReport.players']")
-                    .css('background-color', bgColor);
-            });
+                });
+                $('.clash-rank').each(function (index, obj) {
+                    var fairReport = fairReports_1[index];
+                    $(obj).text(fairReport.fairRank);
+                    if (fairReport.score > 0) {
+                        var bgColor;
+                        switch (fairReport.fairRank) {
+                            case 1:
+                                bgColor = 'mediumseagreen';
+                                break;
+                            case 2:
+                                bgColor = 'yellow';
+                                break;
+                            default: bgColor = 'orange';
+                        }
+                    }
+                    else {
+                        bgColor = 'indianred';
+                    }
+                    // TODO color for Clashing.../Pending... NaN score
+                    $(obj)
+                        .parents("[ng-repeat='player in clashOfCodeService.currentReport.players']")
+                        .css('background-color', bgColor);
+                });
+            }
         }
     }
 }
--- userscript.user.ts
+++ userscript.user.ts
@@ -7,64 +7,79 @@
 // @author      -
 // @description 3/8/2020, 8:42:28 PM
 // @require https://cdnjs.cloudflare.com/ajax/libs/ramda/0.27.1/ramda.min.js
-// @require https://raw.githubusercontent.com/lodash/lodash/4.17.15-npm/core.js
+// @require https://raw.githubusercontent.com/lodash/lodash/4.17.15-npm/lodash.js
 // ==/UserScript==
+
+// TODO
+// * easy way to view code side-by-side
 
 (new MutationObserver(check)).observe(document, {childList: true, subtree: true});
 
 function check(_changes, observer) {
     if(document.querySelector('.player-report')) {
         observer.disconnect();
-        let reports = [];
-        $('.player-report').each((_, obj) =>
-        {
-            reports.push({
-                rank:  parseInt($(obj).find('.clash-rank').text()),
-                score: parseInt($(obj).find('div.info-clash.score > div > div.info-content-container > div.info-value > span').text()),
-                nickname: $(obj).find('.nickname').text(),
-                language: $(obj).find('div.info-clash.language > div > div.info-content-container > div.info-value > span').text()
-            });
-        });
 
-        let isShortestMode = $('div.clash-info-container > div > div.info-clash.criterion > div > div.info-content-container > div.info-label > span').first().text() === 'CHARACTERS';
+        var previousFinishedCount = 0;
+        setInterval(update, 3000);
 
-        if (isShortestMode){
-            let reportsByLanguage = R.groupBy<any>(report => report.language)(reports);
-            R.forEachObjIndexed((reports, _language) => {
-                R.addIndex<any>(R.forEach)((report, idx) => report.fairRank = idx + 1, reports);
-            }, reportsByLanguage)
-
-            let fairReports =
-                _.sortBy(_.flatten(_.map(reportsByLanguage, (reports, _) => reports)), report => report.rank);
-
-            let worstRank = _.max(_.map(fairReports, report => report.fairRank));
-            _.forEach(fairReports, report =>{
-                if (report.score === 0){
-                    report.fairRank = worstRank + 1
-                }
-            });
-
-            $('.clash-rank').each((index, obj) =>
+        function update() {
+            let reports = [];
+            $('.player-report').each((_, obj) =>
             {
-                let fairReport = fairReports[index];
-                $(obj).text(fairReport.fairRank);
-                if (fairReport.score > 0){
-                    var bgColor;
-                    switch (fairReport.fairRank) {
-                        case 1: bgColor = 'mediumseagreen'; break;
-                        case 2: bgColor = 'yellow'; break;
-                        default: bgColor = 'orange';
-                    }
-                }
-                else
-                {
-                    bgColor = 'indianred'
-                }
+                reports.push({
+                    rank:  parseInt($(obj).find('.clash-rank').text()),
+                    score: parseInt($(obj).find('div.info-clash.score > div > div.info-content-container > div.info-value > span').text()),
+                    nickname: $(obj).find('.nickname').text(),
+                    language: $(obj).find('div.info-clash.language > div > div.info-content-container > div.info-value > span').text()
+                });
+            });
 
-                $(obj)
-                    .parents("[ng-repeat='player in clashOfCodeService.currentReport.players']")
-                    .css('background-color', bgColor);
-            })
+            var finishedCount = _.countBy(reports, report => !isNaN(report.rank)).true;
+            if (previousFinishedCount === finishedCount) return;
+            previousFinishedCount = finishedCount;
+    
+            let isShortestMode = $('div.clash-info-container > div > div.info-clash.criterion > div > div.info-content-container > div.info-label > span').first().text() === 'CHARACTERS';
+    
+            if (isShortestMode){
+                let reportsByLanguage = R.groupBy<any>(report => report.language)(reports);
+                R.forEachObjIndexed((reports, _language) => {
+                    R.addIndex<any>(R.forEach)((report, idx) => report.fairRank = idx + 1, reports);
+                }, reportsByLanguage)
+    
+                let fairReports =
+                    _.sortBy(_.flatten(_.map(reportsByLanguage, (reports, _) => reports)), report => report.rank);
+    
+                let worstRank = _.max(_.map(fairReports, report => report.fairRank));
+                _.forEach(fairReports, report =>{
+                    if (report.score === 0){
+                        report.fairRank = worstRank + 1
+                    }
+                });
+    
+                $('.clash-rank').each((index, obj) =>
+                {
+                    let fairReport = fairReports[index];
+                    $(obj).text(fairReport.fairRank);
+                    if (fairReport.score > 0){
+                        var bgColor;
+                        switch (fairReport.fairRank) {
+                            case 1: bgColor = 'mediumseagreen'; break;
+                            case 2: bgColor = 'yellow'; break;
+                            default: bgColor = 'orange';
+                        }
+                    }
+                    else
+                    {
+                        bgColor = 'indianred'
+                    }
+    
+                    // TODO color for Clashing.../Pending... NaN score
+    
+                    $(obj)
+                        .parents("[ng-repeat='player in clashOfCodeService.currentReport.players']")
+                        .css('background-color', bgColor);
+                })
+            }
         }
     }
 }
